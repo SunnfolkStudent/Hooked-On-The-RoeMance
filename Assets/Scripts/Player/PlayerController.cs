062d446dc349ff65c8f983a8839087c9ac9477fd
WiDuck1 <87336958+WiDuck1@users.noreply.github.com>
2024-12-05 13:43:44 +0100

    Fishing mechanic, new player idle, etc

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")] 
     [SerializeField] private float moveSpeed = 5f;
     
+    [Header("Biomes")]
+    private bool _isInOcean, _isInDeepsea, _isInTropical, _isInColdwater;
+    
     private void Start()
     {
         _input = GetComponent<InputActions>();
@@ -24,18 +27,11 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         AdjustPlayerFacingDirection();
-
+        
+        FishingMechanic();
+        
         _animator.SetFloat("moveY", _input.movement.y);
         _animator.SetFloat("moveX", _input.movement.x);
-        
-
-        /*
-        if (!PauseMenu._menuActive)
-        {
-            _animator.SetFloat("moveY", _input.movement.y);
-            _animator.SetFloat("moveX", _input.movement.x);
-        }
-        */
     }
     
     private void FixedUpdate()
@@ -48,7 +44,6 @@ public class PlayerController : MonoBehaviour
         if (_input.movement.x < 0)
         {
             _spriteRenderer.flipX = false;
-            //PlayerFlipX = true;   Might not be needed
         }
         
         if (_input.movement.x > 0)
@@ -57,4 +52,68 @@ public class PlayerController : MonoBehaviour
         }
     }
     
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Ocean":
+                _isInOcean = true;
+                _isInColdwater = false;
+                _isInDeepsea = false;
+                _isInTropical = false;
+                break;
+            case "Deepsea":
+                _isInOcean = false;
+                _isInColdwater = false;
+                _isInDeepsea = true;
+                _isInTropical = false;
+                break;
+            case "Tropical":
+                _isInOcean = false;
+                _isInColdwater = false;
+                _isInDeepsea = false;
+                _isInTropical = true;
+                break;
+            case "Coldwater":
+                _isInOcean = false;
+                _isInColdwater = true;
+                _isInDeepsea = false;
+                _isInTropical = false;
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isInOcean = false;
+        _isInColdwater = false;
+        _isInDeepsea = false;
+        _isInTropical = false;
+    }
+    
+    private void FishingMechanic()
+    {
+        if (_isInOcean && _input.interact)
+        {
+            print("FishingOcean");
+        }
+        if (_isInDeepsea && _input.interact)
+        {
+            print("FishingDeepsea");
+        }
+        if (_isInColdwater && _input.interact)
+        {
+            print("FishingColdwater");
+        }
+        if (_isInTropical && _input.interact)
+        {
+            print("FishingTropical");
+        }
+        
+        /*
+         Pseudo-code:
+         Randomly get one of the scriptable object fishes and trash item from one of the specific biomes
+         */
+        
+    }
 }
