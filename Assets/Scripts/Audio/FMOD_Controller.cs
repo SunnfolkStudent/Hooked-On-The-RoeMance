f391c7f07465c0d24bc303c6788eb505c4eeb39f
Robin B. <70322796+RoboOlaf@users.noreply.github.com>
2024-12-12 13:42:38 +0100

    Anger

--- Assets/Scripts/Audio/FMOD_Controller.cs
+++ Assets/Scripts/Audio/FMOD_Controller.cs
@@ -1,15 +1,41 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using FMODUnity;
+using FMOD.Studio;
+using FMOD;
+using Debug = UnityEngine.Debug;
 
 public class FMOD_Controller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    FMOD.Studio.EventInstance musicInstance;
+
+    public static FMOD_Controller instance;
+    private void Start()
     {
+        musicInstance = RuntimeManager.CreateInstance("event:/Music/mx_generalMusic");
+        musicInstance.start();
         
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        
+        if (sceneName == "Erik_Test")
+        {
+            Debug.Log("Scene Robin");
+            musicInstance.setParameterByName("Main Music", 1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager");
+        }
+        instance = this;
+    }
+
+    private void Update()
     {
         
     }
