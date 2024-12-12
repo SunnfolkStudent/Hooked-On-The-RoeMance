7446001b220e45237ad37ac0cefc07c9e389bbcd
Robin B. <70322796+RoboOlaf@users.noreply.github.com>
2024-12-12 13:29:13 +0100

    instance fix

--- Assets/Scripts/Audio/AudioManagerController.cs
+++ Assets/Scripts/Audio/AudioManagerController.cs
@@ -10,6 +10,7 @@ public class AudioManagerController : MonoBehaviour
 {
     FMOD.Studio.EventInstance musicInstance;
 
+    public static AudioManagerController instance;
     private void Start()
     {
         musicInstance = RuntimeManager.CreateInstance("event:/Music/mx_generalMusic");
@@ -27,7 +28,11 @@ public class AudioManagerController : MonoBehaviour
 
     private void Awake()
     {
-        
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager");
+        }
+        instance = this;
     }
 
     private void Update()
