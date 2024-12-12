5dbce014cd7f83bcbd5bf1f39fcd1704089e1b25
KELR04 <Kaspersyvertsen04@gmail.com>
2024-12-12 14:58:35 +0100

    Update TypewriterTest.cs

--- Assets/Scripts/Dialogue/TypewriterTest.cs
+++ Assets/Scripts/Dialogue/TypewriterTest.cs
@@ -155,88 +155,88 @@ public class TypewriterTest : MonoBehaviour
         {
             case 0:
                 currentFish = Fish.Shark;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(4);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(4);
                 break;
             case 1:
                 currentFish = Fish.Macarel;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(8);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(8);
                 break;
             case 2:
                 currentFish = Fish.Jellyfish;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(5);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(5);
                 break;
             case 3:
                 currentFish = Fish.Halibut;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(7);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(7);
                 break;
             case 4:
                 currentFish = Fish.Angler;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(0);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(0);
                 break;
             case 5:
                 currentFish = Fish.Blobfish;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(1);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(1);
                 break;
             case 6:
                 currentFish = Fish.Macarel;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(8);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(8);
                 break;
             case 7:
                 currentFish = Fish.Halibut;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(7);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(7);
                 break;
             case 8:
                 currentFish = Fish.Halibut;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(7);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(7);
                 break;
             case 9:
                 currentFish = Fish.Koi;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(6);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(6);
                 break;
             case 10:
                 currentFish = Fish.Macarel;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(8);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(8);
                 break;    
             case 11:
                 currentFish = Fish.Clown;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(3);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(3);
                 break;
             case 12:
                 currentFish = Fish.Octopus;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(9);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(9);
                 break;
             case 13:
                 currentFish = Fish.Clam;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(2);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(2);
                 break;
             case 14:
                 currentFish = Fish.Macarel;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(8);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(8);
                 break;
             case 15:
                 currentFish = Fish.Halibut;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(7);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(7);
                 break;
             case 16:
                 currentFish = Fish.Trash;
-                _FMOD.FishingMode(2);
-                _FMOD.CharacterTheme(10);
+                // _FMOD.FishingMode(2);
+                // _FMOD.CharacterTheme(10);
                 break;
         }
         // Debug.Log("enum is " + currentFish);
